820e2e6f4f03b8d51588bea5fcede58f5cd16df5
Wizqqr <132731212+Wizqqr@users.noreply.github.com>
2025-06-03 21:28:42 +0600

    Render

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Аналог __dirname для ES-модулей
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
