c7f369a9cd3d93e2de93be7ff32c6d388351b084
Wizqqr <132731212+Wizqqr@users.noreply.github.com>
2025-06-03 21:33:21 +0600

    Render

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-// Аналог __dirname для ES-модулей
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
